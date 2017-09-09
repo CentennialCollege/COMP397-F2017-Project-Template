0bd02ef48094b7001401cfc98cfdcbb589045908
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-09-09 16:12:09 -0400

    added references to createjs in index.html

--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,17 +1,31 @@
 // IIFE - Immediately Invoked Function Expression
 (function(){
-
+  let stage:createjs.Stage;
+  let canvas:any;
+  let helloLabel:createjs.Text;
 
   function Start() {
-    console.log("Game Started...");
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.on("tick", Update);
+    Main();
   }
 
   function Update() {
-
+    helloLabel.rotation += 5;
+    stage.update();
   }
 
   function Main() {
+    console.log("Game Started...");
 
+    helloLabel = new createjs.Text("Hello, World!", "40px Consolas", "#000000");
+    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
+    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
+    helloLabel.x = 320;
+    helloLabel.y = 240;
+    stage.addChild(helloLabel);
   }
 
   window.onload = Start;
