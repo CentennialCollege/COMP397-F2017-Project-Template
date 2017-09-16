21ee245048f5f44e40322aaaf2057e377f964d8a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-09-16 15:04:53 -0400

    added clickMeButton

---
+++ Scripts/objects/button.ts
@@ -0,0 +1,12 @@
+module objects {
+  export class Button extends createjs.Bitmap {
+    // PRIVATE INSTANCE VARIABlES +++++++++++
+    // PUBLIC PROPERTIES ++++++++++++++++++++
+    // CONSTRUCTORS +++++++++++++++++++++++++
+    constructor() {
+      super();
+    }
+    // PRIVATE METHODS ++++++++++++++++++++++
+    // PUBLIC METHODS +++++++++++++++++++++++
+  }
+}
