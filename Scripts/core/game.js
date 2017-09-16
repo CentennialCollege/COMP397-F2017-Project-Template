bc08fcee2fd2288619b577bb2735a049046ad7ea
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-09-16 14:51:50 -0400

    added Label object

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -3,6 +3,7 @@
     var stage;
     var canvas;
     var helloLabel;
+    var goodByeLabel;
     function Start() {
         canvas = document.getElementById("canvas");
         stage = new createjs.Stage(canvas);
@@ -12,16 +13,15 @@
     }
     function Update() {
         helloLabel.rotation += 5;
+        goodByeLabel.rotation -= 5;
         stage.update();
     }
     function Main() {
         console.log("Game Started...");
-        helloLabel = new createjs.Text("Good Bye, World!", "40px Consolas", "#000000");
-        helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-        helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-        helloLabel.x = 320;
-        helloLabel.y = 240;
+        helloLabel = new objects.Label("Hello World!", "40px", "Consolas", "#000000", 320, 240, true);
         stage.addChild(helloLabel);
+        goodByeLabel = new objects.Label("Good Bye!", "40px", "Consolas", "#FF0000", 320, 240, true);
+        stage.addChild(goodByeLabel);
     }
     window.onload = Start;
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -2,7 +2,8 @@
 (function(){
   let stage:createjs.Stage;
   let canvas:any;
-  let helloLabel:createjs.Text;
+  let helloLabel:objects.Label;
+  let goodByeLabel:objects.Label;
 
   function Start() {
     canvas = document.getElementById("canvas");
@@ -14,18 +15,18 @@
 
   function Update() {
     helloLabel.rotation += 5;
+    goodByeLabel.rotation -= 5;
     stage.update();
   }
 
   function Main() {
     console.log("Game Started...");
 
-    helloLabel = new createjs.Text("Good Bye, World!", "40px Consolas", "#000000");
-    helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
-    helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
-    helloLabel.x = 320;
-    helloLabel.y = 240;
+    helloLabel = new objects.Label("Hello World!", "40px", "Consolas", "#000000", 320, 240, true);
     stage.addChild(helloLabel);
+
+    goodByeLabel = new objects.Label("Good Bye!", "40px", "Consolas", "#FF0000", 320, 240, true);
+    stage.addChild(goodByeLabel);
   }
 
   window.onload = Start;
